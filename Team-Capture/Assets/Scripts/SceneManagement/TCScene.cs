432e89ece503256936538a04a285d7297dab399c
Voltstro <me@voltstro.dev>
2020-01-01 21:24:39 +1000

    Cleaned up TCScene.cs

--- Team-Capture/Assets/Scripts/SceneManagement/TCScene.cs
+++ Team-Capture/Assets/Scripts/SceneManagement/TCScene.cs
@@ -7,18 +7,24 @@ namespace SceneManagement
 	[CreateAssetMenu(fileName = "New TC Scene", menuName = "Team Capture/TCScene")]
 	public class TCScene : ScriptableObject
 	{
-		public string displayName;
-
-		[Tooltip("Is this scene playable?")] public bool enabled = true;
-
-		public GamemodeSettings gamemodeSettings;
-		public float hitObjectLastTime = 2.0f;
-
-		public float respawnTime = 4.0f;
+		[Header("Basic Scene Settings")]
 		[Scene] public string sceneName;
 
+		[Tooltip("Its 'nice' name")]
+		public string displayName;
+
+		[Tooltip("Is this scene playable?")] 
+		public bool enabled = true;
+
+		[Header("Game Settings")]
+		public GamemodeSettings gamemodeSettings;
+		public float respawnTime = 4.0f;
+
+		[Tooltip("What weapons do you want the player to start with?")]
 		public TCWeapon[] stockWeapons;
 
+		[Header("Temp Stuff")]
+		public float hitObjectLastTime = 2.0f;
 		public GameObject weaponHit;
 	}
 }
