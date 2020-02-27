5f3e67e794211a5bd904e44d95492ecd321b1c28
Voltstro <me@voltstro.dev>
2020-02-27 14:36:40 +1000

    Added Volume(Post Porcessing)

---
+++ Team-Capture/Assets/Scripts/Settings/VolumeDontDestroy.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Settings
+{
+	public class VolumeDontDestroy : MonoBehaviour
+	{
+		private VolumeDontDestroy Instance;
+
+		private void Awake()
+		{
+			if (Instance != null)
+			{
+				Destroy(gameObject);
+				return;
+			}
+
+			Instance = this;
+			DontDestroyOnLoad(gameObject);
+		}
+	}
+}
