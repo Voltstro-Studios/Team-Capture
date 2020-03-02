f0e15f2c96bae42c0aefd80363ec93fc534df6f2
Voltstro <me@voltstro.dev>
2020-03-02 10:47:38 +1000

    Added options menu base prefab

--- Team-Capture/Assets/Scripts/Settings/VolumeDontDestroy.cs
+++ Team-Capture/Assets/Scripts/Settings/VolumeDontDestroy.cs
@@ -1,11 +1,15 @@
 ﻿using UnityEngine;
+using UnityEngine.Rendering;
 using Logger = Core.Logger.Logger;
 
 namespace Settings
 {
+	[RequireComponent(typeof(Volume))]
 	public class VolumeDontDestroy : MonoBehaviour
 	{
 		private static VolumeDontDestroy instance;
+
+		public static Volume ActiveVolume;
 
 		private void Awake()
 		{
@@ -17,6 +21,8 @@ namespace Settings
 
 			instance = this;
 			DontDestroyOnLoad(gameObject);
+
+			ActiveVolume = GetComponent<Volume>();
 		}
 
 		private void Start()
---
+++ Team-Capture/Assets/Scripts/UI/Panels/OptionsPanel.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+namespace UI.Panels
+{
+	public class OptionsPanel : MainMenuPanelBase
+	{
+		[SerializeField] private GameObject keyboardMenu;
+		[SerializeField] private GameObject videoMenu;
+		[SerializeField] private GameObject advVideoMenu;
+		[SerializeField] private GameObject otherMenu;
+
+		private void Start()
+		{
+			OpenKeyboardMenu();
+		}
+
+		public void OpenKeyboardMenu()
+		{
+			CloseAll();
+			keyboardMenu.SetActive(true);
+		}
+
+		public void OpenVideoMenu()
+		{
+			CloseAll();
+			videoMenu.SetActive(true);
+		}
+
+		public void OpenAdvVideoMenu()
+		{
+			CloseAll();
+			advVideoMenu.SetActive(true);
+		}
+
+		public void OpenOtherMenu()
+		{
+			CloseAll();
+			otherMenu.SetActive(true);
+		}
+
+		private void CloseAll()
+		{
+			keyboardMenu.SetActive(false);
+			videoMenu.SetActive(false);
+			advVideoMenu.SetActive(false);
+			otherMenu.SetActive(false);
+		}
+	}
+}
