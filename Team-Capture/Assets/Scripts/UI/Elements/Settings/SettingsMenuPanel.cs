0a10ccedcb0a30855f0eeba0b4c45008f2691709
Voltstro <me@voltstro.dev>
2020-03-05 14:50:07 +1000

    Moved SettingsMenuPanel.cs to its proper spot

---
+++ Team-Capture/Assets/Scripts/UI/Elements/Settings/SettingsMenuPanel.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace UI.Elements.Settings
+{
+	public class SettingsMenuPanel : MonoBehaviour
+	{
+		[SerializeField] private Transform scrollingArea;
+
+		public Transform GetScrollingArea => scrollingArea;
+	}
+}
--- Team-Capture/Assets/SettingsMenuPanel.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-public class SettingsMenuPanel : MonoBehaviour
-{
-	[SerializeField] private Transform scrollingArea;
-
-	public Transform GetScrollingArea => scrollingArea;
-}
