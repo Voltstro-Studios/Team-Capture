d0cb4fb1a725bbc75c1d2b375e6102ebffbc6469
EternalClickbait <48875125+EternalClickbait@users.noreply.github.com>
2020-03-06 12:32:14 +1000

    Move easter eggs

---
+++ Team-Capture/Assets/Scripts/Misc/EasterEggs.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using Logger = Core.Logger.Logger;
+
+namespace Misc
+{
+	[DefaultExecutionOrder(-2000)]
+	public class EasterEggs : MonoBehaviour
+	{
+		private void Awake()
+		{
+			DontDestroyOnLoad(gameObject);
+		}
+
+		private void Update()
+		{
+			if (!Input.GetKey(KeyCode.W)) return;
+			if (!Input.GetKey(KeyCode.A)) return;
+			if (!Input.GetKey(KeyCode.S)) return;
+			if (!Input.GetKey(KeyCode.D)) return;
+			// ReSharper disable once CompareOfFloatsByEqualityOperator
+			if (quitAt != -1) return;
+			quitAt = Time.unscaledTime + 2f;
+			Logger.Log(QuitMessage);
+		}
+
+		private static float quitAt = -1;
+		private const string QuitMessage = "Don't go pressing keys when you don't know what they do";
+
+		public void OnGUI()
+		{
+			// ReSharper disable once CompareOfFloatsByEqualityOperator
+			if (quitAt == -1) return;
+			if (Time.unscaledTime > quitAt)
+			{
+				
+			}
+#if UNITY_EDITOR
+			GUI.TextArea(new Rect(0, 0, Screen.width, Screen.height), QuitMessage, EditorStyles.boldLabel);
+#endif
+		}
+	}
+}
---
+++ Team-Capture/Assets/Scripts/Misc/Editor/EasterEggsEditor.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+
+namespace Misc
+{
+	[CustomEditor(typeof(EasterEggs))]
+	public class EasterEggsEditor : Editor
+	{
+		public override void OnInspectorGUI()
+		{
+			EditorGUILayout.HelpBox("Well done for finding this script!", MessageType.None);
+			base.OnInspectorGUI();
+		}
+	}
+}
