b97aca2daadcb7c0ba9884bf4c5350da461624ce
EternalClickbait <48875125+EternalClickbait@users.noreply.github.com>
2020-03-06 13:06:21 +1000

    Update EasterEggs.cs

--- Team-Capture/Assets/Scripts/Misc/EasterEggs.cs
+++ Team-Capture/Assets/Scripts/Misc/EasterEggs.cs
@@ -1,17 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using UnityEditor;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.Diagnostics;
 using Logger = Core.Logger.Logger;
+using Random = System.Random;
 
 namespace Misc
 {
 	[DefaultExecutionOrder(-2000)]
 	public class EasterEggs : MonoBehaviour
 	{
+		private const string QuitMessage = "Don't go pressing keys when you don't know what they do";
+		private static KeyCode[] quitKeys = {KeyCode.W, KeyCode.A, KeyCode.S, KeyCode.D};
+		private static float quitAt = -1;
+
+		[Range(1, 5)] [SerializeField] private int randomKeyLength = -1;
+
 		private void Awake()
 		{
 			DontDestroyOnLoad(gameObject);
+			if (randomKeyLength != -1)
+			{
+				quitKeys = new KeyCode[randomKeyLength];
+				for (int i = 0; i < randomKeyLength; i++)
+				{
+					quitKeys[i] = (KeyCode) new Random().Next(60, 200);
+					Debug.Log($"Quit key {i + 1} is {quitKeys[i]}");
+				}
+			}
 		}
 
 		private void Update()
@@ -26,20 +40,19 @@ namespace Misc
 			Logger.Log(QuitMessage);
 		}
 
-		private static float quitAt = -1;
-		private const string QuitMessage = "Don't go pressing keys when you don't know what they do";
-
 		public void OnGUI()
 		{
 			// ReSharper disable once CompareOfFloatsByEqualityOperator
 			if (quitAt == -1) return;
-			if (Time.unscaledTime > quitAt)
+			//Draws a red box to cover up the screen
+			GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), new Texture2D(1, 1), ScaleMode.StretchToFill, false, 1, Color.red, 0, 0);
+			GUI.TextArea(new Rect(0, 0, Screen.width, Screen.height), QuitMessage, new GUIStyle
 			{
-				
-			}
-#if UNITY_EDITOR
-			GUI.TextArea(new Rect(0, 0, Screen.width, Screen.height), QuitMessage, EditorStyles.boldLabel);
-#endif
+				fontStyle = FontStyle.BoldAndItalic, fontSize = 100, wordWrap = true, alignment = TextAnchor.MiddleCenter
+			});
+
+			//Crash because fuck you that's why
+			if (Time.unscaledTime > quitAt) Utils.ForceCrash(ForcedCrashCategory.Abort);
 		}
 	}
 }
