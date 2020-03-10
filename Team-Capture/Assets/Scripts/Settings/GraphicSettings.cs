129db8f02e3817ef0a417bcafba4f7d22f115ee9
Voltstro <me@voltstro.dev>
2020-03-10 10:48:22 +1000

    Added 'r_resolution' command

--- Team-Capture/Assets/Scripts/Settings/GraphicSettings.cs
+++ Team-Capture/Assets/Scripts/Settings/GraphicSettings.cs
@@ -1,4 +1,6 @@
-﻿using Settings.SettingClasses;
+﻿using Attributes;
+using Core.Logger;
+using Settings.SettingClasses;
 using UnityEngine;
 using Logger = Core.Logger.Logger;
 
@@ -18,6 +20,30 @@ namespace Settings
 			Screen.SetResolution(settings.Resolution.width, settings.Resolution.height, settings.ScreenMode, settings.Resolution.refreshRate);
 
 			Logger.Log("Applied video settings");
+		}
+
+		[ConCommand("r_resolution", "Set the resolution (width x height)", 2, 2)]
+		public static void SetResolution(string[] args)
+		{
+			if (int.TryParse(args[0], out int widthRes))
+			{
+				if (int.TryParse(args[1], out int heightRes))
+				{
+					GameSettings.VideoSettings.Resolution = new Resolution
+					{
+						height = heightRes,
+						width = widthRes,
+						refreshRate = Screen.currentResolution.refreshRate
+					};
+
+					GameSettings.Save();
+
+					return;
+				}
+			}
+
+			Logger.Log("Invalid input!", LogVerbosity.Error);
+
 		}
 	}
 }
