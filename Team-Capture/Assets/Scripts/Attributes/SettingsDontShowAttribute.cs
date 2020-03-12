8d2bbcea2227e4feb79c36b9a7376111ac0cc2e1
Voltstro <me@voltstro.dev>
2020-03-12 10:08:28 +1000

    XML

--- Team-Capture/Assets/Scripts/Attributes/SettingsDontShowAttribute.cs
+++ Team-Capture/Assets/Scripts/Attributes/SettingsDontShowAttribute.cs
@@ -2,6 +2,9 @@
 
 namespace Attributes
 {
+	/// <summary>
+	/// Doesn't show this property or field in the settings menu
+	/// </summary>
 	[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Class)]
 	public class SettingsDontShowAttribute : Attribute
 	{
--- Team-Capture/Assets/Scripts/Attributes/SettingsPropertyFormatNameAttribute.cs
+++ Team-Capture/Assets/Scripts/Attributes/SettingsPropertyFormatNameAttribute.cs
@@ -2,6 +2,10 @@
 
 namespace Attributes
 {
+	/// <summary>
+	/// Tells the <see cref="Settings.DynamicSettingsUi"/> what the text should be, instead of the property name.
+	/// <para>If this isn't applied, the <see cref="Settings.DynamicSettingsUi"/> will just use the field name.</para>
+	/// </summary>
 	[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
 	public class SettingsPropertyFormatNameAttribute : Attribute
 	{
