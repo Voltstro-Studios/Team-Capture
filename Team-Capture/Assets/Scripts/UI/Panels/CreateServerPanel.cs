542986f7468498151dfa6d8cacab35a286e31be5
Voltstro <me@voltstro.dev>
2020-08-29 23:27:40 +1000

    Added some XML and comments to CreateServerPanel

--- Team-Capture/Assets/Scripts/UI/Panels/CreateServerPanel.cs
+++ Team-Capture/Assets/Scripts/UI/Panels/CreateServerPanel.cs
@@ -9,42 +9,74 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using Debug = UnityEngine.Debug;
+using Logger = Core.Logging.Logger;
 
 namespace UI.Panels
 {
+	/// <summary>
+	/// The code driving the create server panel
+	/// </summary>
 	public class CreateServerPanel : MainMenuPanelBase
 	{
-		private List<TCScene> activeTCScenes;
+		private List<TCScene> onlineTCScenes;
 
-		public TMP_Dropdown mapsDropdown;
-
-		public TMP_InputField gameNameText;
 		private Image gameNameImage;
-
-		public TMP_InputField maxPlayersText;
 		private Image maxPlayersImage;
 
-		public Color errorColor = Color.red;
+		private Color gameNameImageColor;
+		private Color maxPlayersImageColor;
 
 		private int maxPlayers = 16;
 
 		private TCNetworkManager netManager;
 
+		/// <summary>
+		/// Dropdown for the maps
+		/// </summary>
+		[Tooltip("Dropdown for the maps")]
+		public TMP_Dropdown mapsDropdown;
+
+		/// <summary>
+		/// Input for the game name
+		/// </summary>
+		[Tooltip("Input for the game name")]
+		public TMP_InputField gameNameText;
+
+		/// <summary>
+		/// Input for the max amount of players
+		/// </summary>
+		[Tooltip("Input for the max amount of players")]
+		public TMP_InputField maxPlayersText;
+
+		/// <summary>
+		/// The color to use when there is an error
+		/// </summary>
+		[Tooltip("The color to use when there is an error")]
+		public Color errorColor = Color.red;
+
 		private void Start()
 		{
+			//First, clear the maps dropdown
 			mapsDropdown.ClearOptions();
 
-			activeTCScenes = TCScenesManager.GetAllEnabledOnlineScenesInfo().ToList();
+			//Then get all online scenes
+			onlineTCScenes = TCScenesManager.GetAllEnabledOnlineScenesInfo().ToList();
 
-			List<string> scenes = activeTCScenes.Select(scene => scene.displayName).ToList();
-
+			//And all the scenes to the map dropdown
+			List<string> scenes = onlineTCScenes.Select(scene => scene.displayName).ToList();
 			mapsDropdown.AddOptions(scenes);
 			mapsDropdown.RefreshShownValue();
 
+			//Get active network manager
 			netManager = TCNetworkManager.Instance;
 
+			//Get the images that are in the input fields
 			gameNameImage = gameNameText.GetComponent<Image>();
 			maxPlayersImage = maxPlayersText.GetComponent<Image>();
+
+			//Get the existing colors of the input fields
+			gameNameImageColor = gameNameImage.color;
+			maxPlayersImageColor = maxPlayersImage.color;
 		}
 
 		/// <summary>
@@ -53,6 +85,7 @@ namespace UI.Panels
 		public void CreateGame()
 		{
 #if UNITY_EDITOR
+			//If we are running as the editor, then we to check to see if an existing build already exists and use that instead
 			if (!System.IO.Directory.Exists($"{VoltBuilder.BuildTool.GetBuildFolder()}Team-Capture-Quick/"))
 			{
 				Debug.LogError("There is no pre-existing build of Team-Capture! Build the game using VoltBuild.");
@@ -60,25 +93,30 @@ namespace UI.Panels
 			}
 #endif
 
+			//Make sure the game name isn't white space or null
 			if (string.IsNullOrWhiteSpace(gameNameText.text))
 			{
+				Logger.Error("Game name input is white space or null!");
 				gameNameImage.color = errorColor;
 				return;
 			}
 
+			//Make sure the max players input is actually a number
 			if (int.TryParse(maxPlayersText.text, out int result))
 			{
 				//Make sure max players is greater then 1
 				if (result <= 1)
 				{
+					Logger.Error("Max players must be greater then one!");
 					maxPlayersImage.color = errorColor;
 					return;
 				}
 
 				maxPlayers = result;
 			}
-			else
+			else //Display an error if is not a number
 			{
+				Logger.Error("Max players input isn't just an int!");
 				maxPlayersImage.color = errorColor;
 				return;
 			}
@@ -89,6 +127,7 @@ namespace UI.Panels
 				return;
 			}
 
+			//Now start the server
 			Process newTcServer = new Process
 			{
 				StartInfo = new ProcessStartInfo
@@ -100,17 +139,18 @@ namespace UI.Panels
 #else
 					FileName = "Team-Capture",
 #endif
-					Arguments = $"-batchmode -nographics -name \"{gameNameText.text}\" -scene {activeTCScenes[mapsDropdown.value].SceneFileName}"
+					Arguments = $"-batchmode -nographics -name \"{gameNameText.text}\" -scene {onlineTCScenes[mapsDropdown.value].SceneFileName}"
 				}
 			};
 			newTcServer.Start();
 
+			//And start the client, it should auto connect once the server is up
 			netManager.StartClient();
 		}
 
 		private void StartServer()
 		{
-			netManager.onlineScene = activeTCScenes[mapsDropdown.value].scene;
+			netManager.onlineScene = onlineTCScenes[mapsDropdown.value].scene;
 			netManager.gameName = gameNameText.text;
 			netManager.maxConnections = maxPlayers;
 			netManager.StartHost();
@@ -127,14 +167,14 @@ namespace UI.Panels
 
 		public void ResetGameNameTextColor()
 		{
-			if(gameNameImage.color == errorColor)
-				gameNameText.GetComponent<Image>().color = Color.white;
+			if (gameNameImage.color == errorColor)
+				gameNameImage.color = gameNameImageColor;
 		}
 
 		public void ResetMaxPlayersTextColor()
 		{
-			if(maxPlayersImage.color == errorColor)
-				maxPlayersImage.color = Color.white;
+			if (maxPlayersImage.color == errorColor)
+				maxPlayersImage.color = maxPlayersImageColor;
 		}
 	}
 }
