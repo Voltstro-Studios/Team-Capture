c6234daaf9f313c12059b19c8b0c15e3425b5ccc
Voltstro <me@voltstro.dev>
2020-08-29 23:09:37 +1000

    TCScene XML
    * Removed some un-needed files

--- Team-Capture/Assets/Scripts/SceneManagement/TCScene.cs
+++ Team-Capture/Assets/Scripts/SceneManagement/TCScene.cs
@@ -4,38 +4,82 @@ using UnityEngine;
 
 namespace SceneManagement
 {
+	/// <summary>
+	/// Represents a Team-Capture scene data
+	/// </summary>
 	[CreateAssetMenu(fileName = "New TC Scene", menuName = "Team Capture/TCScene")]
 	public class TCScene : ScriptableObject
 	{
+		/// <summary>
+		/// The actual Unity scene
+		/// </summary>
 		[Header("Basic Scene Settings")] 
 		[Scene] public string scene;
 
+		/// <summary>
+		/// The scene, but only its name (So no 'Assets/Scenes/*.unity' stuff)
+		/// </summary>
 		public string SceneFileName => Path.GetFileNameWithoutExtension(scene);
 
-		[Tooltip("Its 'nice' name")] 
+		/// <summary>
+		/// The display name, the name that will be shown to the user
+		/// </summary>
+		[Tooltip("The display name, the name that will be shown to the user")] 
 		public string displayName;
 
+		/// <summary>
+		/// Will this scene be included in the build?
+		/// </summary>
 		[Tooltip("Will this scene be included in the build?")] 
 		public bool enabled = true;
 
-		[Tooltip("Is this scene an online scene, the only offline one should be Main Menus and such")]
+		/// <summary>
+		/// Is this scene an online scene, the only offline one should be the Main Menu or Bootloader and such
+		/// </summary>
+		[Tooltip("Is this scene an online scene, the only offline one should be the Main Menu or Bootloader and such")]
 		public bool isOnlineScene = true;
 
+		/// <summary>
+		/// Can this scene be loaded to directly (Using the scene command)
+		/// </summary>
+		[Tooltip("Can this scene be loaded to directly (Using the scene command)")]
 		public bool canLoadTo = true;
 
+		/// <summary>
+		/// The parent object that will hold all the pickups
+		/// </summary>
+		[Tooltip("The parent object that will hold all the pickups")]
 		public string pickupsParent = "/Pickups/";
 
-		[Header("Game Settings")] 
-		public GamemodeSettings gamemodeSettings;
-		
+		/// <summary>
+		/// The time until the players respawn
+		/// </summary>
+		[Header("Game Settings")]
+		[Tooltip("The time until the players respawn")]
 		public float respawnTime = 4.0f;
 
+		/// <summary>
+		/// Do you want to show the start time on Discord RPC
+		/// </summary>
 		[Header("Discord RPC")] 
 		public bool showStartTime = true;
 
+		/// <summary>
+		/// Is this the main menu?
+		/// </summary>
+		[Tooltip("Is this the main menu?")]
 		public bool isMainMenu;
 
+		/// <summary>
+		/// What large image to use
+		/// </summary>
+		[Tooltip("What large image to use")]
 		public string largeImageKey = "tc_icon";
+
+		/// <summary>
+		/// What text will the large image have
+		/// </summary>
+		[Tooltip("What text will the large image have")]
 		public string largeImageKeyText = "Team Capture";
 	}
 }
