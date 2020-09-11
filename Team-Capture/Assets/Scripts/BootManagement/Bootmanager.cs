7361ef0fc6deb595ecc5e4439a41f6d3d44854d8
Voltstro <me@voltstro.dev>
2020-09-11 12:28:20 +1000

    Bootmanager HasBooted

--- Team-Capture/Assets/Scripts/BootManagement/Bootmanager.cs
+++ Team-Capture/Assets/Scripts/BootManagement/Bootmanager.cs
@@ -2,13 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using Logger = Core.Logging.Logger;
 
 namespace BootManagement
 {
 	public class Bootmanager : MonoBehaviour
 	{
+		public static bool HasBooted;
+
 		private void Awake()
 		{
+			//No needed to do stuff if this has been booted already
+			if (HasBooted)
+			{
+				Destroy();
+				return;
+			}
+
 			//First, find all scripts that inherit from IStartOnBoot
 			IEnumerable<Type> types = AppDomain.CurrentDomain.GetAssemblies().SelectMany(x => x.GetTypes())
 				.Where(x => typeof(IStartOnBoot).IsAssignableFrom(x) && !x.IsInterface);
@@ -27,6 +37,16 @@ namespace BootManagement
 					//Ignore
 				}
 			}
+
+			Logger.Info("Bootloader has successfully loaded!");
+
+			HasBooted = true;
+			Destroy();
+		}
+
+		private void Destroy()
+		{
+			Destroy(gameObject);
 		}
 	}
 }
