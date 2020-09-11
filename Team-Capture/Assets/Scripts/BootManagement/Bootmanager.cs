2cd1ff0edee3255ba675892323f855f56642251c
Voltstro <me@voltstro.dev>
2020-09-11 12:23:09 +1000

    Try and Catch

--- Team-Capture/Assets/Scripts/BootManagement/Bootmanager.cs
+++ Team-Capture/Assets/Scripts/BootManagement/Bootmanager.cs
@@ -15,11 +15,17 @@ namespace BootManagement
 
 			foreach (Type type in types)
 			{
-				GameObject newBootedObject = new GameObject(type.Name);
-				IStartOnBoot bootedScript = (IStartOnBoot)newBootedObject.AddComponent(type);
-				bootedScript.Init();
-
-				Debug.Log(type.Name);
+				try
+				{
+					//Create a new game object and add the script to it
+					GameObject newBootedObject = new GameObject(type.Name);
+					IStartOnBoot bootedScript = (IStartOnBoot) newBootedObject.AddComponent(type);
+					bootedScript.Init();
+				}
+				catch
+				{
+					//Ignore
+				}
 			}
 		}
 	}
