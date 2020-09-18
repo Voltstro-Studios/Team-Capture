c86d87d24af2f5b80b8e773662152a2659bcf04c
Voltstro <me@voltstro.dev>
2020-09-18 20:58:18 +1000

    Added some more XML

--- Team-Capture/Assets/Scripts/Settings/Enums/TextureQuality.cs
+++ Team-Capture/Assets/Scripts/Settings/Enums/TextureQuality.cs
@@ -1,10 +1,28 @@
 ﻿namespace Settings.Enums
 {
+	/// <summary>
+	/// The texture quality
+	/// </summary>
 	public enum TextureQuality
 	{
+		/// <summary>
+		/// Full resolution textures
+		/// </summary>
 		FullRes = 1,
+
+		/// <summary>
+		/// Half resolution textures
+		/// </summary>
 		HalfRes = 2,
+
+		/// <summary>
+		/// Quarter resolution textures
+		/// </summary>
 		QuarterRes = 3,
+
+		/// <summary>
+		/// Eight resolution textures
+		/// </summary>
 		EightRes = 4
 	}
 }
--- Team-Capture/Assets/Scripts/Settings/Enums/VSync.cs
+++ Team-Capture/Assets/Scripts/Settings/Enums/VSync.cs
@@ -1,9 +1,23 @@
 ﻿namespace Settings.Enums
 {
+	/// <summary>
+	/// VSync options
+	/// </summary>
 	public enum VSync
 	{
+		/// <summary>
+		/// No VSync, this is best in high action games
+		/// </summary>
 		Disable = 0,
+
+		/// <summary>
+		/// Sync the framerate to the framerate of your monitor
+		/// </summary>
 		EveryVBlank = 1,
+
+		/// <summary>
+		/// Half the framerate of your monitor
+		/// </summary>
 		EverySecondVBlank = 2
 	}
 }
