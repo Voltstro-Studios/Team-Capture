1239eabd89b8e2942bb8f59d1e9c8b4b32c5fc25
Voltstro <me@voltstro.dev>
2020-09-24 16:09:47 +1000

    Move UtilCommands into Console

---
+++ Team-Capture/Assets/Scripts/Console/UtilCommands.cs
@@ -0,0 +1,62 @@
+﻿using System.IO;
+using Core;
+using Core.Logging;
+
+namespace Console
+{
+	/// <summary>
+	/// A bunch of util commands
+	/// </summary>
+	public static class UtilCommands
+	{
+		private const string SplashScreenResourceFile = "Resources/console-splashscreen.txt";
+
+		[ConCommand("quit", "Quits the game")]
+		public static void QuitGameCommand(string[] args)
+		{
+			Game.QuitGame();
+		}
+
+		[ConCommand("echo", "Echos back what you type in")]
+		public static void EchoCommand(string[] args)
+		{
+			Logger.Info(string.Join(" ", args));
+		}
+
+		[ConCommand("asciiart", "Shows Team-Capture ascii art")]
+		public static void AsciiArtCommand(string[] args)
+		{
+			//Ascii art, fuck you
+			const string asciiArt = @"
+___________                    
+\__    ___/___ _____    _____  
+  |    |_/ __ \\__  \  /     \ 
+  |    |\  ___/ / __ \|  Y Y  \
+  |____| \___  >____  /__|_|  /
+             \/     \/      \/ 
+	_________                __                        
+	\_   ___ \_____  _______/  |_ __ _________   ____  
+	/    \  \/\__  \ \____ \   __\  |  \_  __ \_/ __ \ 
+	\     \____/ __ \|  |_> >  | |  |  /|  | \/\  ___/ 
+	 \______  (____  /   __/|__| |____/ |__|    \___  >
+	        \/     \/|__|                           \/ 
+";
+			Logger.Info(asciiArt);
+		}
+
+		[ConCommand("splashmessage", "Shows a random splash message")]
+		public static void SplashMessageCommand(string[] args)
+		{
+			//Random splash message
+			string splashMessagesPath = $"{Game.GetGameExecutePath()}/{SplashScreenResourceFile}";
+			if (File.Exists(splashMessagesPath))
+			{
+				string[] lines = File.ReadAllLines(splashMessagesPath);
+
+				//Select random number
+				int index = UnityEngine.Random.Range(0, lines.Length);
+				Logger.Info($"	{lines[index]}");
+			}
+		}
+	}
+}
