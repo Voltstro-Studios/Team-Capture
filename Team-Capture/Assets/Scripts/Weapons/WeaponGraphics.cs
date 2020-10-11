f62af4b22d8e78fe31be70950cbee0a7a5af21ab
Voltstro <me@voltstro.dev>
2020-10-11 19:53:39 +1000

    Added cl_muzzleflashlighting ConVar

--- Team-Capture/Assets/Scripts/Weapons/WeaponGraphics.cs
+++ Team-Capture/Assets/Scripts/Weapons/WeaponGraphics.cs
@@ -1,9 +1,37 @@
-﻿using UnityEngine;
+﻿using System;
+using Console;
+using UnityEngine;
 
 namespace Weapons
 {
 	public class WeaponGraphics : MonoBehaviour
 	{
+		[ConVar("cl_muzzleflashlighting", "Whether or not the muzzle flash will have lighting", nameof(InvokeLightingChange))]
+		public static bool MuzzleFlashLighting = true;
+		public static event Action LightingChange;
+
+		public static void InvokeLightingChange()
+		{
+			LightingChange?.Invoke();
+		}
+
+		public void ChangeLighting()
+		{
+			ParticleSystem.LightsModule lighting = muzzleFlash.lights;
+			lighting.enabled = MuzzleFlashLighting;
+		}
+
+		public void OnEnable()
+		{
+			LightingChange += ChangeLighting;
+			ChangeLighting();
+		}
+
+		public void OnDisable()
+		{
+			LightingChange -= ChangeLighting;
+		}
+
 		public ParticleSystem muzzleFlash;
 		public Transform bulletTracerPosition;
 	}
