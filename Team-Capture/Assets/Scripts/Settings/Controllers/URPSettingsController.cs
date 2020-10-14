e4662a09604e8cc7a1c98dfd5b809e4ad985b149
Voltstro <me@voltstro.dev>
2020-10-14 11:12:04 +1000

    Added commands for URP

--- Team-Capture/Assets/Scripts/Settings/Controllers/URPSettingsController.cs
+++ Team-Capture/Assets/Scripts/Settings/Controllers/URPSettingsController.cs
@@ -1,4 +1,5 @@
-﻿using Core;
+﻿using Console;
+using Core;
 using Settings.URPSettings;
 using UnityEngine;
 using UnityEngine.Rendering;
@@ -38,5 +39,79 @@ namespace Settings.Controllers
 
 			Logger.Info("Applied URP settings.");
 		}
+
+		#region Console Commands
+
+		[ConCommand("r_shadow_distance", "Sets the distance of the shadows", CommandRunPermission.ClientOnly, 1, 1, true)]
+		public static void SetShadowDistanceCommand(string[] args)
+		{
+			if (int.TryParse(args[0], out int result))
+			{
+				GameSettings.AdvSettings.ShadowDistance = result;
+				GameSettings.Save();
+
+				return;
+			}
+
+			Logger.Error("Invalid input!");
+		}
+
+		[ConCommand("r_shadow_cascades", "Sets the shadow cascades", CommandRunPermission.ClientOnly, 1, 1, true)]
+		public static void SetShadowCascadesCommand(string[] args)
+		{
+			if (int.TryParse(args[0], out int result))
+			{
+				GameSettings.AdvSettings.ShadowCascades = (ShadowCascadesOption) result;
+				GameSettings.Save();
+
+				return;
+			}
+
+			Logger.Error("Invalid input!");
+		}
+
+		[ConCommand("r_render_scale", "Sets the render scale", CommandRunPermission.ClientOnly, 1, 1, true)]
+		public static void SetRenderScaleCommand(string[] args)
+		{
+			if (float.TryParse(args[0], out float result))
+			{
+				GameSettings.AdvSettings.RenderScale = result;
+				GameSettings.Save();
+
+				return;
+			}
+
+			Logger.Error("Invalid input!");
+		}
+
+		[ConCommand("r_msaa_quality", "Sets the msaa quality", CommandRunPermission.ClientOnly, 1, 1, true)]
+		public static void SetMsaaQualityCommand(string[] args)
+		{
+			if (int.TryParse(args[0], out int result))
+			{
+				GameSettings.AdvSettings.MsaaQuality = (MSAAQuality) result;
+				GameSettings.Save();
+
+				return;
+			}
+
+			Logger.Error("Invalid input!");
+		}
+
+		[ConCommand("r_hdr", "Sets HDR", CommandRunPermission.ClientOnly, 1, 1, true)]
+		public static void SetHdrCommand(string[] args)
+		{
+			if (bool.TryParse(args[0], out bool result))
+			{
+				GameSettings.AdvSettings.HDR = result;
+				GameSettings.Save();
+
+				return;
+			}
+
+			Logger.Error("Invalid input!");
+		}
+
+		#endregion
 	}
 }
