88f5e25cac8216836e0f1267b3fb60cc854acab0
Voltstro <me@voltstro.dev>
2020-10-26 10:03:17 +1000

    Added XML to BulletTracer.cs

--- Team-Capture/Assets/Scripts/Weapons/BulletTracer.cs
+++ Team-Capture/Assets/Scripts/Weapons/BulletTracer.cs
@@ -2,10 +2,21 @@
 
 namespace Weapons
 {
+	/// <summary>
+	/// The line thingy that goes were the bullets go that we fake, because it looks
+	/// </summary>
 	internal class BulletTracer : MonoBehaviour
 	{
+		/// <summary>
+		/// The bullet tracer <see cref="ParticleSystem"/>
+		/// </summary>
+		[Tooltip("The bullet tracer Particle System")]
 		[SerializeField] private ParticleSystem bulletTracer;
 
+		/// <summary>
+		/// Plays the bullet tracer particle
+		/// </summary>
+		/// <param name="target"></param>
 		public void Play(Vector3 target)
 		{
 			transform.LookAt(target);
