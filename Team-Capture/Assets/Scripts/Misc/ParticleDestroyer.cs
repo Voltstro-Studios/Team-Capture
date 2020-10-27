2b22d81251f86c19b8c154140988ab1621dda63a
Voltstro <me@voltstro.dev>
2020-10-27 14:38:14 +1000

    Update ParticleDestroyer.cs

--- Team-Capture/Assets/Scripts/Misc/ParticleDestroyer.cs
+++ Team-Capture/Assets/Scripts/Misc/ParticleDestroyer.cs
@@ -7,6 +7,9 @@ namespace Misc
 	/// </summary>
 	public class ParticleDestroyer : MonoBehaviour
 	{
+		/// <summary>
+		/// What delay to use until destroy
+		/// </summary>
 		[SerializeField] private float destroyDelayTime = 2.0f;
 
 		private void Start()
