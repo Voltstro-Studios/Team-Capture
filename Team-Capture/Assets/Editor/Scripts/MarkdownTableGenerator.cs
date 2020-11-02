51dd8d8cbea7ea3eabf493fb930ee5a6a15ff0a3
Voltstro <me@voltstro.dev>
2020-11-02 19:20:38 +1000

    Moved some editor scripts around
    * Added start of new MarkdownTableGenerator util

---
+++ Team-Capture/Assets/Editor/Scripts/MarkdownTableGenerator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+
+namespace Editor.Scripts
+{
+	public class MarkdownTableGenerator
+	{
+		private readonly StringBuilder stringBuilder;
+		private readonly int headersCount;
+
+		public MarkdownTableGenerator(string tile, params string[] headers)
+		{
+			if(headers.Length == 0)
+				throw new ArgumentOutOfRangeException(nameof(headers));
+
+			headersCount = headers.Length;
+
+			stringBuilder = new StringBuilder();
+			stringBuilder.Append($"## {tile}\n\n|");
+
+			string headerUnderline = "\n|";
+
+			foreach (string header in headers)
+			{
+				stringBuilder.Append($"{header}|");
+				headerUnderline += $"{new string('-', header.Length)}|";
+			}
+
+			stringBuilder.Append($"{headerUnderline}\n");
+		}
+
+		public void AddOption(params string[] content)
+		{
+			if(content.Length != headersCount)
+				throw new ArgumentOutOfRangeException(nameof(content), "The content count doesn't meet how many header there are!");
+
+			stringBuilder.Append("|");
+
+			foreach (string option in content)
+			{
+				stringBuilder.Append($"{option}|");
+			}
+
+			stringBuilder.Append("\n");
+		}
+
+		public override string ToString()
+		{
+			return stringBuilder.ToString();
+		}
+	}
+}
---
+++ Team-Capture/Assets/Editor/Tests/EditorUtilsTest.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Editor.Scripts;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class EditorUtilsTest
+    {
+	    #region Header Tests
+
+	    [Test]
+	    public void MarkdownTableGenerator2HeadersTest()
+	    {
+		    MarkdownTableGenerator generator = new MarkdownTableGenerator("Test", "Header1", "Header2");
+		    StringAssert.AreEqualIgnoringCase("## Test\n\n|Header1|Header2|\n|-------|-------|\n", generator.ToString());
+	    }
+
+	    [Test]
+	    public void MarkdownTableGenerator3HeadersTest()
+	    {
+		    MarkdownTableGenerator generator = new MarkdownTableGenerator("Test", "Header1", "Header2", "Header3");
+		    StringAssert.AreEqualIgnoringCase("## Test\n\n|Header1|Header2|Header3|\n|-------|-------|-------|\n", generator.ToString());
+	    }
+
+		
+	    #endregion
+
+	    #region Option Tests
+
+	    [Test]
+	    public void MarkdownTableGeneratorAdd1Option2Headers()
+	    {
+		    MarkdownTableGenerator generator = new MarkdownTableGenerator("Test", "Header1", "Header2");
+			generator.AddOption("Test1", "Test2");
+			StringAssert.AreEqualIgnoringCase("## Test\n\n|Header1|Header2|\n|-------|-------|\n|Test1|Test2|\n", generator.ToString());
+	    }
+
+	    [Test]
+	    public void MarkdownTableGeneratorAdd2Option2Headers()
+	    {
+		    MarkdownTableGenerator generator = new MarkdownTableGenerator("Test", "Header1", "Header2");
+		    generator.AddOption("Test1", "Test2");
+		    generator.AddOption("Test3", "Test4");
+		    StringAssert.AreEqualIgnoringCase("## Test\n\n|Header1|Header2|\n|-------|-------|\n|Test1|Test2|\n|Test3|Test4|\n", generator.ToString());
+	    }
+
+	    [Test]
+	    public void MarkdownTableGeneratorAdd1Option3Headers()
+	    {
+		    MarkdownTableGenerator generator = new MarkdownTableGenerator("Test", "Header1", "Header2", "Header3");
+		    generator.AddOption("Test1", "Test2", "Test3");
+		    StringAssert.AreEqualIgnoringCase("## Test\n\n|Header1|Header2|Header3|\n|-------|-------|-------|\n|Test1|Test2|Test3|\n", generator.ToString());
+	    }
+
+	    [Test]
+	    public void MarkdownTableGeneratorAdd2Option3Headers()
+	    {
+		    MarkdownTableGenerator generator = new MarkdownTableGenerator("Test", "Header1", "Header2", "Header3");
+		    generator.AddOption("Test1", "Test2", "Test3");
+		    generator.AddOption("Test4", "Test5", "Test6");
+		    StringAssert.AreEqualIgnoringCase("## Test\n\n|Header1|Header2|Header3|\n|-------|-------|-------|\n|Test1|Test2|Test3|\n|Test4|Test5|Test6|\n", generator.ToString());
+	    }
+
+	    [Test]
+	    public void MarkdownTableGeneratorTooManyOptions()
+	    {
+		    Assert.Throws(typeof(ArgumentOutOfRangeException), () =>
+		    {
+			    MarkdownTableGenerator generator = new MarkdownTableGenerator("Test", "Header1", "Header2", "Header3");
+				generator.AddOption("Test1", "Test2");
+		    });
+	    }
+		
+	    #endregion
+    }
+}
