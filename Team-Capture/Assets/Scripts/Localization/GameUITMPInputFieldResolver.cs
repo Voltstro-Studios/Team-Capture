201bba28632213557f5ac75a04081879904e53a9
Voltstro <me@voltstro.dev>
2020-12-06 17:47:15 +1000

    Locales for create server menu

---
+++ Team-Capture/Assets/Scripts/Localization/GameUITMPInputFieldResolver.cs
@@ -0,0 +1,14 @@
+﻿using Localization;
+using TMPro;
+using UnityEngine;
+
+[RequireComponent(typeof(TMP_InputField))]
+public class GameUITMPInputFieldResolver : MonoBehaviour
+{
+	private void Start()
+	{
+		TMP_InputField input = GetComponent<TMP_InputField>();
+		input.text = GameUILocale.ResolveString(input.text);
+		Destroy(this);
+	}
+}
