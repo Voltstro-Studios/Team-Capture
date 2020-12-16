0b1fa96fc26445134c46281831504d7a77f14e54
Voltstro <me@voltstro.dev>
2020-12-17 03:31:42 +1000

    Added PacketCompressionTests

---
+++ Team-Capture/Assets/Editor/Tests/PacketCompressionTests.cs
@@ -0,0 +1,30 @@
+using Core.Networking;
+using Core.Networking.MessagePack;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace Editor.Tests
+{
+	public class PacketCompressionTests
+	{
+		[Test]
+		public void PacketSerializeAndDeserializeTest()
+		{
+			int targetsSize = 100;
+			WeaponShootEffectsTargets testMessage = new WeaponShootEffectsTargets
+			{
+				Targets = new Vector3[targetsSize], TargetNormals = new Vector3[targetsSize]
+			};
+
+			for (int i = 0; i < targetsSize; i++)
+			{
+				testMessage.Targets[i] = new Vector3(1.0f, 1.0f, 1.0f);
+				testMessage.TargetNormals[i] = new Vector3(2.0f, 2.0f, 2.0f);
+			}
+
+			byte[] bytes = PacketCompression.CompressMessage(testMessage);
+
+			PacketCompression.ReadMessage<WeaponShootEffectsTargets>(bytes);
+		}
+	}
+}
