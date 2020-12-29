d76c99260c82e2978dcc5adff29c6b1aaa68fb15
Voltstro <me@voltstro.dev>
2020-12-30 00:28:59 +1000

    Added Open Screenshot Directory

--- Team-Capture/Assets/Editor/Scripts/ScreenshotMenuItems.cs
+++ Team-Capture/Assets/Editor/Scripts/ScreenshotMenuItems.cs
@@ -1,8 +1,10 @@
 using System;
+using System.Diagnostics;
 using System.IO;
 using Team_Capture.Core;
 using UnityEditor;
 using UnityEngine;
+using Debug = UnityEngine.Debug;
 
 namespace Team_Capture.Editor
 {
@@ -13,12 +15,25 @@ namespace Team_Capture.Editor
 		[MenuItem("Screenshot/Take Screenshot")]
 	    public static void TakeScreenshot()
 	    {
-		    if (!Directory.Exists(screenshotDir))
-			    Directory.CreateDirectory(screenshotDir);
+		    CheckScreenshotDirectory();
 
 		    string fileName = $"{screenshotDir}{DateTime.Now:yy-MM-dd-hh-mm-ss}.png";
 			ScreenCapture.CaptureScreenshot(fileName);
 			Debug.Log($"Saved screenshot to {fileName}");
+	    }
+
+	    [MenuItem("Screenshot/Open Screenshot Directory")]
+	    public static void OpenScreenshotDirectory()
+	    {
+		    CheckScreenshotDirectory();
+
+		    Process.Start(screenshotDir);
+	    }
+
+	    private static void CheckScreenshotDirectory()
+	    {
+		    if (!Directory.Exists(screenshotDir))
+			    Directory.CreateDirectory(screenshotDir);
 	    }
     }
 }
