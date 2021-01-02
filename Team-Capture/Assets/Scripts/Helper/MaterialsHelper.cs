235d7747b7e7ba712b0baf2c011b006b47d27a64
Voltstro <me@voltstro.dev>
2021-01-02 20:59:38 +1000

    Update MaterialsHelper.cs

--- Team-Capture/Assets/Scripts/Helper/MaterialsHelper.cs
+++ Team-Capture/Assets/Scripts/Helper/MaterialsHelper.cs
@@ -1,17 +1,32 @@
 using System;
 using UnityEngine;
 
+// ReSharper disable IdentifierTypo
+// ReSharper disable StringLiteralTypo
 namespace Team_Capture.Helper
 {
+	/// <summary>
+	///		Provides helper for materials
+	/// </summary>
     public static class MaterialsHelper
     {
-	    public enum SurfaceType
+		//Material surface type change code based off from here:
+		//https://answers.unity.com/questions/1608815/change-surface-type-with-lwrp.html
+
+	    private static readonly int Surface = Shader.PropertyToID("_Surface");
+	    private static readonly int Blend = Shader.PropertyToID("_Blend");
+	    private static readonly int AlphaClip = Shader.PropertyToID("_AlphaClip");
+	    private static readonly int SrcBlend = Shader.PropertyToID("_SrcBlend");
+	    private static readonly int DstBlend = Shader.PropertyToID("_DstBlend");
+	    private static readonly int ZWrite = Shader.PropertyToID("_ZWrite");
+
+	    private enum SurfaceType
 	    {
 		    Opaque,
 		    Transparent
 	    }
 
-	    public enum BlendMode
+	    private enum BlendMode
 	    {
 		    Alpha,
 		    Premultiply,
@@ -19,80 +34,90 @@ namespace Team_Capture.Helper
 		    Multiply
 	    }
 
+		/// <summary>
+		///		Changes the <see cref="Material"/>'s transparency
+		/// </summary>
+		/// <param name="material"></param>
+		/// <param name="transparent"></param>
 	    public static void ChangeMaterialTransparency(this Material material, bool transparent)
 	    {
 		    if (transparent)
 		    {
-			    material.SetFloat("_Surface", (float)SurfaceType.Transparent);
-			    material.SetFloat("_Blend", (float)BlendMode.Alpha);
+			    material.SetFloat(Surface, (float)SurfaceType.Transparent);
+			    material.SetFloat(Blend, (float)BlendMode.Alpha);
 		    }
 		    else
 		    {
-			    material.SetFloat("_Surface", (float)SurfaceType.Opaque);
+			    material.SetFloat(Surface, (float)SurfaceType.Opaque);
 		    }
 
 		    material.SetupMaterialBlend();
 	    }
 
+		/// <summary>
+		///		Sets up <see cref="Material"/>'s blend
+		/// </summary>
+		/// <param name="material"></param>
 	    public static void SetupMaterialBlend(this Material material)
 	    {
 			if (material == null)
 				throw new ArgumentNullException(nameof(material));
 
-		    bool alphaClip = material.GetFloat("_AlphaClip") == 1;
+		    bool alphaClip = material.GetFloat(AlphaClip) == 1;
 		    if (alphaClip)
 		         material.EnableKeyword("_ALPHATEST_ON");
 		    else
 			    material.DisableKeyword("_ALPHATEST_ON");
 
-		    SurfaceType surfaceType = (SurfaceType)material.GetFloat("_Surface");
+		    SurfaceType surfaceType = (SurfaceType)material.GetFloat(Surface);
 		    if (surfaceType == 0)
 		    {
 			    material.SetOverrideTag("RenderType", "");
-			    material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
-			    material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
-			    material.SetInt("_ZWrite", 1);
+			    material.SetInt(SrcBlend, (int)UnityEngine.Rendering.BlendMode.One);
+			    material.SetInt(DstBlend, (int)UnityEngine.Rendering.BlendMode.Zero);
+			    material.SetInt(ZWrite, 1);
 			    material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
 			    material.renderQueue = -1;
 			    material.SetShaderPassEnabled("ShadowCaster", true);
 		    }
 		    else
 		    {
-			    BlendMode blendMode = (BlendMode)material.GetFloat("_Blend");
+			    BlendMode blendMode = (BlendMode)material.GetFloat(Blend);
 			    switch (blendMode)
 			    {
 				    case BlendMode.Alpha:
 					    material.SetOverrideTag("RenderType", "Transparent");
-					    material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
-					    material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
-					    material.SetInt("_ZWrite", 0);
+					    material.SetInt(SrcBlend, (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+					    material.SetInt(DstBlend, (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+					    material.SetInt(ZWrite, 0);
 					    material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
 					    material.renderQueue = (int)UnityEngine.Rendering.RenderQueue.Transparent;
 					    material.SetShaderPassEnabled("ShadowCaster", false);
 					    break;
 				    case BlendMode.Premultiply:
 					    material.SetOverrideTag("RenderType", "Transparent");
-					    material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
-					    material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
-					    material.SetInt("_ZWrite", 0);
+					    material.SetInt(SrcBlend, (int)UnityEngine.Rendering.BlendMode.One);
+					    material.SetInt(DstBlend, (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+					    material.SetInt(ZWrite, 0);
 					    material.EnableKeyword("_ALPHAPREMULTIPLY_ON");
 					    material.renderQueue = (int)UnityEngine.Rendering.RenderQueue.Transparent;
 					    material.SetShaderPassEnabled("ShadowCaster", false);
 					    break;
 				    case BlendMode.Additive:
 					    material.SetOverrideTag("RenderType", "Transparent");
-					    material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
-					    material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.One);
-					    material.SetInt("_ZWrite", 0);
+					    material.SetInt(SrcBlend, (int)UnityEngine.Rendering.BlendMode.One);
+					    material.SetInt(DstBlend, (int)UnityEngine.Rendering.BlendMode.One);
+					    material.SetInt(ZWrite, 0);
 					    material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
 					    material.renderQueue = (int)UnityEngine.Rendering.RenderQueue.Transparent;
 					    material.SetShaderPassEnabled("ShadowCaster", false);
 					    break;
 				    case BlendMode.Multiply:
 					    material.SetOverrideTag("RenderType", "Transparent");
-					    material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.DstColor);
-					    material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
-					    material.SetInt("_ZWrite", 0);
+					    material.SetInt(SrcBlend, (int)UnityEngine.Rendering.BlendMode.DstColor);
+					    material.SetInt(DstBlend, (int)UnityEngine.Rendering.BlendMode.Zero);
+					    material.SetInt(ZWrite, 0);
+					    
 					    material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
 					    material.renderQueue = (int)UnityEngine.Rendering.RenderQueue.Transparent;
 					    material.SetShaderPassEnabled("ShadowCaster", false);
@@ -102,3 +127,6 @@ namespace Team_Capture.Helper
 	    }
     }
 }
+
+// ReSharper restore StringLiteralTypo
+// ReSharper restore IdentifierTypo
