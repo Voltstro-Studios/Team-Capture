c89356bfbbb0d0989533b93a140acccf38ff7514
Voltstro <me@voltstro.dev>
2021-01-12 00:38:17 +1000

    Update Logger

--- Team-Capture/Assets/Scripts/Core/Logging/Logger.cs
+++ Team-Capture/Assets/Scripts/Core/Logging/Logger.cs
@@ -114,49 +114,8 @@ namespace Team_Capture.Core.Logging
 			if (!IsLoggerInitialized)
 				throw new InitializationException("The logger isn't initialized!");
 
-			log.Debug(message);
-		}
-
-		/// <summary>
-		///     Writes a debug log
-		/// </summary>
-		/// <param name="message"></param>
-		/// <param name="value"></param>
-		public static void Debug<T>(string message, T value)
-		{
-			if (!IsLoggerInitialized)
-				throw new InitializationException("The logger isn't initialized!");
-
-			log.Debug(message, value);
-		}
-
-		/// <summary>
-		///     Writes a debug log
-		/// </summary>
-		/// <param name="message"></param>
-		/// <param name="value0"></param>
-		/// <param name="value1"></param>
-		public static void Debug<T>(string message, T value0, T value1)
-		{
-			if (!IsLoggerInitialized)
-				throw new InitializationException("The logger isn't initialized!");
-
-			log.Debug(message, value0, value1);
-		}
-
-		/// <summary>
-		///     Writes a debug log
-		/// </summary>
-		/// <param name="message"></param>
-		/// <param name="value0"></param>
-		/// <param name="value1"></param>
-		/// <param name="value2"></param>
-		public static void Debug<T>(string message, T value0, T value1, T value2)
-		{
-			if (!IsLoggerInitialized)
-				throw new InitializationException("The logger isn't initialized!");
-
-			log.Debug(message, value0, value1, value2);
+			if(DebugLogMode)
+				log.Debug(message);
 		}
 
 		/// <summary>
@@ -169,7 +128,8 @@ namespace Team_Capture.Core.Logging
 			if (!IsLoggerInitialized)
 				throw new InitializationException("The logger isn't initialized!");
 
-			log.Debug(message, values);
+			if(DebugLogMode)
+				log.Debug(message, values);
 		}
 
 		#endregion
@@ -186,48 +146,6 @@ namespace Team_Capture.Core.Logging
 				throw new InitializationException("The logger isn't initialized!");
 
 			log.Information(message);
-		}
-
-		/// <summary>
-		///     Writes an information log
-		/// </summary>
-		/// <param name="message"></param>
-		/// <param name="value"></param>
-		public static void Info<T>(string message, T value)
-		{
-			if (!IsLoggerInitialized)
-				throw new InitializationException("The logger isn't initialized!");
-
-			log.Information(message, value);
-		}
-
-		/// <summary>
-		///     Writes an information log
-		/// </summary>
-		/// <param name="message"></param>
-		/// <param name="value0"></param>
-		/// <param name="value1"></param>
-		public static void Info<T>(string message, T value0, T value1)
-		{
-			if (!IsLoggerInitialized)
-				throw new InitializationException("The logger isn't initialized!");
-
-			log.Information(message, value0, value1);
-		}
-
-		/// <summary>
-		///     Writes an information log
-		/// </summary>
-		/// <param name="message"></param>
-		/// <param name="value0"></param>
-		/// <param name="value1"></param>
-		/// <param name="value2"></param>
-		public static void Info<T>(string message, T value0, T value1, T value2)
-		{
-			if (!IsLoggerInitialized)
-				throw new InitializationException("The logger isn't initialized!");
-
-			log.Information(message, value0, value1, value2);
 		}
 
 		/// <summary>
@@ -263,48 +181,6 @@ namespace Team_Capture.Core.Logging
 		///     Writes a warning log
 		/// </summary>
 		/// <param name="message"></param>
-		/// <param name="value"></param>
-		public static void Warn<T>(string message, T value)
-		{
-			if (!IsLoggerInitialized)
-				throw new InitializationException("The logger isn't initialized!");
-
-			log.Warning(message, value);
-		}
-
-		/// <summary>
-		///     Writes a warning log
-		/// </summary>
-		/// <param name="message"></param>
-		/// <param name="value0"></param>
-		/// <param name="value1"></param>
-		public static void Warn<T>(string message, T value0, T value1)
-		{
-			if (!IsLoggerInitialized)
-				throw new InitializationException("The logger isn't initialized!");
-
-			log.Warning(message, value0, value1);
-		}
-
-		/// <summary>
-		///     Writes a warning log
-		/// </summary>
-		/// <param name="message"></param>
-		/// <param name="value0"></param>
-		/// <param name="value1"></param>
-		/// <param name="value2"></param>
-		public static void Warn<T>(string message, T value0, T value1, T value2)
-		{
-			if (!IsLoggerInitialized)
-				throw new InitializationException("The logger isn't initialized!");
-
-			log.Warning(message, value0, value1, value2);
-		}
-
-		/// <summary>
-		///     Writes a warning log
-		/// </summary>
-		/// <param name="message"></param>
 		/// <param name="values"></param>
 		public static void Warn(string message, params object[] values)
 		{
@@ -334,48 +210,6 @@ namespace Team_Capture.Core.Logging
 		///     Writes an error log
 		/// </summary>
 		/// <param name="message"></param>
-		/// <param name="value"></param>
-		public static void Error<T>(string message, T value)
-		{
-			if (!IsLoggerInitialized)
-				throw new InitializationException("The logger isn't initialized!");
-
-			log.Error(message, value);
-		}
-
-		/// <summary>
-		///     Writes an error log
-		/// </summary>
-		/// <param name="message"></param>
-		/// <param name="value0"></param>
-		/// <param name="value1"></param>
-		public static void Error<T>(string message, T value0, T value1)
-		{
-			if (!IsLoggerInitialized)
-				throw new InitializationException("The logger isn't initialized!");
-
-			log.Error(message, value0, value1);
-		}
-
-		/// <summary>
-		///     Writes an error log
-		/// </summary>
-		/// <param name="message"></param>
-		/// <param name="value0"></param>
-		/// <param name="value1"></param>
-		/// <param name="value2"></param>
-		public static void Error<T>(string message, T value0, T value1, T value2)
-		{
-			if (!IsLoggerInitialized)
-				throw new InitializationException("The logger isn't initialized!");
-
-			log.Error(message, value0, value1, value2);
-		}
-
-		/// <summary>
-		///     Writes an error log
-		/// </summary>
-		/// <param name="message"></param>
 		/// <param name="values"></param>
 		public static void Error(string message, params object[] values)
 		{
@@ -383,6 +217,33 @@ namespace Team_Capture.Core.Logging
 				throw new InitializationException("The logger isn't initialized!");
 
 			log.Error(message, values);
+		}
+
+		/// <summary>
+		///     Writes an error log
+		/// </summary>
+		/// <param name="exception"></param>
+		/// <param name="message"></param>
+		public static void Error(Exception exception, string message)
+		{
+			if (!IsLoggerInitialized)
+				throw new InitializationException("The logger isn't initialized!");
+
+			log.Error(exception, message);
+		}
+
+		/// <summary>
+		///     Writes an error log
+		/// </summary>
+		/// <param name="exception"></param>
+		/// <param name="message"></param>
+		/// <param name="values"></param>
+		public static void Error(Exception exception, string message, params object[] values)
+		{
+			if (!IsLoggerInitialized)
+				throw new InitializationException("The logger isn't initialized!");
+
+			log.Error(exception, message, values);
 		}
 
 		#endregion
