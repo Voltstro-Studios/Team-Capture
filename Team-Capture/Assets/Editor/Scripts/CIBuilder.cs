74a5251f49312f1d17db10a0bb3eeb0bfe412315
Voltstro <me@voltstro.dev>
2021-01-18 03:29:43 +1000

    Updated Volt Builder

--- Team-Capture/Assets/Editor/Scripts/CIBuilder.cs
+++ Team-Capture/Assets/Editor/Scripts/CIBuilder.cs
@@ -1,7 +1,6 @@
 using UnityEditor;
 using UnityEngine;
 using Voltstro.UnityBuilder.Build;
-using Voltstro.UnityBuilder.Settings;
 
 namespace Team_Capture.Editor
 {
@@ -10,14 +9,8 @@ namespace Team_Capture.Editor
         public static void StartVoltBuilder()
         {
 	        BuildTarget target = EditorUserBuildSettings.activeBuildTarget;
-
-            //Yea, yea, we should update Volt Builder to have an API in GameBuilder to change targets, but its 3 AM and I just want this to work for now
-	        UnityEditor.SettingsManagement.Settings voltSettings = SettingsManager.Instance;
-            voltSettings.Set("BuildTarget", target);
-
-			string buildDir = $"{GameBuilder.GetBuildDirectory()}{target}-DevOpsBuild/";
-
-            GameBuilder.BuildGame(buildDir);
+	        string buildDir = $"{GameBuilder.GetBuildDirectory()}{target}-DevOpsBuild/";
+	        GameBuilder.BuildGame(buildDir, target);
 		}
     }
 }
