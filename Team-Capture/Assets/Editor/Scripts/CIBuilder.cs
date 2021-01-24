94b0a0445dd47dcc7c7fc0502353948d4b2ae984
Voltstro <me@voltstro.dev>
2021-01-24 19:42:35 +1000

    Update CIBuilder.cs

--- Team-Capture/Assets/Editor/Scripts/CIBuilder.cs
+++ Team-Capture/Assets/Editor/Scripts/CIBuilder.cs
@@ -1,5 +1,6 @@
+using System;
+using System.Collections.Generic;
 using UnityEditor;
-using UnityEngine;
 using Voltstro.UnityBuilder.Build;
 
 namespace Team_Capture.Editor
@@ -8,9 +9,40 @@ namespace Team_Capture.Editor
     {
         public static void StartVoltBuilder()
         {
-	        BuildTarget target = EditorUserBuildSettings.activeBuildTarget;
-	        string buildDir = $"{GameBuilder.GetBuildDirectory()}{target}-DevOpsBuild/{PlayerSettings.productName}";
+			System.Console.WriteLine("Build game started...");
+
+			ParseCommandLineArguments(out Dictionary<string, string> arguments);
+
+			if(!arguments.ContainsKey("buildTarget"))
+				EditorApplication.Exit(-1);
+
+			BuildTarget target = (BuildTarget) Enum.Parse(typeof(BuildTarget), arguments["buildTarget"]);
+			string buildDir = $"{GameBuilder.GetBuildDirectory()}{target}-DevOpsBuild/{PlayerSettings.productName}";
+
+			System.Console.WriteLine($"Building TC for {target} platform to {buildDir}");
+
 	        GameBuilder.BuildGame(buildDir, target);
 		}
+
+        private static void ParseCommandLineArguments(out Dictionary<string, string> providedArguments)
+        {
+	        providedArguments = new Dictionary<string, string>();
+	        string[] args = Environment.GetCommandLineArgs();
+
+	        // Extract flags with optional values
+	        for (int current = 0, next = 1; current < args.Length; current++, next++) 
+	        {
+		        // Parse flag
+		        bool isFlag = args[current].StartsWith("-");
+		        if (!isFlag) continue;
+
+		        string flag = args[current].TrimStart('-');
+
+		        // Parse optional value
+		        bool flagHasValue = next < args.Length && !args[next].StartsWith("-");
+		        string value = flagHasValue ? args[next].TrimStart('-') : "";
+		        providedArguments.Add(flag, value);
+	        }
+        }
     }
 }
