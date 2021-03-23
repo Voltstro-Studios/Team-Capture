8011b02102eb58efde1fdcf55fe204cf04d5400e
Voltstro <me@voltstro.dev>
2021-03-23 22:53:05 +1000

    Added Steam integration

---
+++ Team-Capture/Assets/Scripts/Integrations/Steamworks/SteamManager.cs
@@ -0,0 +1,79 @@
+using System;
+using System.IO;
+using Steamworks;
+using Team_Capture.Core;
+using Team_Capture.Helper;
+using Team_Capture.Integrations.Discord;
+using Team_Capture.Logging;
+
+namespace Team_Capture.Integrations.Steamworks
+{
+    public class SteamManager : SingletonMonoBehaviour<SteamManager>
+    {
+	    /// <summary>
+	    ///     Where to load the settings from
+	    /// </summary>
+	    public string settingsLocation = "/Resources/Integrations/Steam.json";
+
+	    private SteamManagerSettings settings;
+
+	    protected override void SingletonAwakened()
+	    {
+	    }
+
+	    protected override void SingletonStarted()
+	    {
+		    if (Game.IsHeadless)
+		    {
+			    Destroy(gameObject);
+			    return;
+		    }
+
+		    LoadSettings();
+			Initialize();
+	    }
+
+	    protected override void SingletonDestroyed()
+	    {
+		    SteamClient.Shutdown();
+	    }
+
+	    private void Update()
+	    {
+		    SteamClient.RunCallbacks();
+	    }
+
+	    private void LoadSettings()
+	    {
+		    if (string.IsNullOrWhiteSpace(settingsLocation))
+			    return;
+
+		    settings = ObjectSerializer.LoadJson<SteamManagerSettings>(
+			    Path.GetDirectoryName($"{Game.GetGameExecutePath()}{settingsLocation}"),
+			    $"/{Path.GetFileNameWithoutExtension(settingsLocation)}");
+	    }
+
+	    private void Initialize()
+	    {
+		    try
+		    {
+			    SteamClient.Init(settings.appId);
+		    }
+		    catch (Exception ex)
+		    {
+				Logger.Error(ex, "Something went wrong while starting the Steam integration!");
+				Destroy(gameObject);
+				return;
+		    }
+
+		    if (!SteamClient.IsLoggedOn)
+		    {
+				Logger.Error("Could not connect to Steam!");
+				Destroy(gameObject);
+				return;
+		    }
+
+			Logger.Info("Logged into Steam account {AccountName} with an ID of {AccountID}", SteamClient.Name, SteamClient.SteamId.Value);
+	    }
+    }
+}
---
+++ Team-Capture/Assets/Scripts/Integrations/Steamworks/SteamManagerSettings.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Team_Capture.Integrations.Steamworks
+{
+	[Serializable]
+	internal class SteamManagerSettings
+	{
+		public uint appId;
+	}
+}
