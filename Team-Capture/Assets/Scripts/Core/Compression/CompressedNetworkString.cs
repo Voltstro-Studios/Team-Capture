66fe2ec876e08a01cee7f2b7de21b7ef8382353f
Voltstro <me@voltstro.dev>
2021-05-08 11:16:26 +1000

    XML

--- Team-Capture/Assets/Scripts/Core/Compression/CompressedNetworkString.cs
+++ Team-Capture/Assets/Scripts/Core/Compression/CompressedNetworkString.cs
@@ -2,8 +2,20 @@
 
 namespace Team_Capture.Core.Compression
 {
+    /// <summary>
+    ///     A <see cref="string"/> designed to be sent over the network.
+    ///     <para>
+    ///         This can be very handy for <see cref="string"/>s that are quite long.
+    ///         Shorter <see cref="string"/>s may actually be larger due to the overhead.
+    ///     </para>
+    ///     <para>Uses LZ4 compression.</para>
+    /// </summary>
     public struct CompressedNetworkString
     {
+        /// <summary>
+        ///     Creates a new <see cref="CompressedNetworkString"/> instance
+        /// </summary>
+        /// <param name="string">The <see cref="string"/> to compress</param>
         public CompressedNetworkString(string @string)
         : this()
         {
@@ -12,27 +24,41 @@ namespace Team_Capture.Core.Compression
 
         private byte[] compressedString;
         private string @string;
+        
+        /// <summary>
+        ///     Gets the uncompressed <see cref="string"/>
+        ///     or sets sets the compressed string
+        /// </summary>
         public string String
         {
             get => @string;
             set
             {
                 @string = value;
-                compressedString = Core.Compression.Compression.CompressString(value, out int _).ToArray();
+                compressedString = Compression.CompressString(value, out int _).ToArray();
             }
         }
 
+        /// <summary>
+        ///     Writes the <see cref="CompressedNetworkString"/>
+        /// </summary>
+        /// <param name="writer"><see cref="NetworkWriter"/> to write to</param>
         public void Write(NetworkWriter writer)
         {
             writer.WriteArray(compressedString);
             writer.WriteInt32(@string.Length);
         }
 
+        /// <summary>
+        ///     Reads the <see cref="CompressedNetworkString"/>
+        /// </summary>
+        /// <param name="reader"><see cref="NetworkReader"/> to read from</param>
+        /// <returns></returns>
         public static CompressedNetworkString Read(NetworkReader reader)
         {
             return new CompressedNetworkString
             {
-                @string = Core.Compression.Compression.DecompressString(reader.ReadArray<byte>(), reader.ReadInt32())
+                @string = Compression.DecompressString(reader.ReadArray<byte>(), reader.ReadInt32())
             };
         }
 
