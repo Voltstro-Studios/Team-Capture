35eaf347f9546324437e13154e69f06e53dcf507
Voltstro <me@voltstro.dev>
2021-08-20 19:34:45 +1000

    Start of weapon effects object pooling

---
+++ Team-Capture/Assets/Scripts/Weapons/WeaponEffectsPool.cs
@@ -0,0 +1,51 @@
+﻿// Team-Capture
+// Copyright (C) 2019-2021 Voltstro-Studios
+// 
+// This project is governed by the AGPLv3 License.
+// For more details see the LICENSE file.
+
+using UnityEngine;
+using UnityEngine.Pool;
+
+namespace Team_Capture.Weapons
+{
+    internal sealed class WeaponEffectsPool
+    {
+        private readonly IObjectPool<GameObject> objectPool;
+
+        public WeaponEffectsPool(GameObject prefab)
+        {
+            objectPool = new LinkedPool<GameObject>(() => CreateObject(prefab), OnTakeObject, OnReturnObject, OnDestroyObject);
+        }
+
+        public GameObject GetPooledObject()
+        {
+            return objectPool.Get();
+        }
+
+        public void ReturnPooledObject(GameObject gameObject)
+        {
+            objectPool.Release(gameObject);
+        }
+
+        private GameObject CreateObject(GameObject prefab)
+        {
+            return Object.Instantiate(prefab);
+        }
+
+        private void OnTakeObject(GameObject gameObject)
+        {
+            gameObject.SetActive(true);
+        }
+
+        private void OnReturnObject(GameObject gameObject)
+        {
+            gameObject.SetActive(false);
+        }
+
+        private void OnDestroyObject(GameObject gameObject)
+        {
+            Object.Destroy(gameObject);
+        }
+    }
+}
