0689d89e98001182b36d9387851914917e3153da
Voltstro <me@voltstro.dev>
2021-09-09 21:51:38 +1000

    Update web browser

---
+++ Team-Capture/Assets/Scripts/Logging/TCWebBrowserLogger.cs
@@ -0,0 +1,28 @@
+﻿// Team-Capture
+// Copyright (C) 2019-2021 Voltstro-Studios
+// 
+// This project is governed by the AGPLv3 License.
+// For more details see the LICENSE file.
+
+using UnityWebBrowser.Logging;
+
+namespace Team_Capture.Logging
+{
+    public class TCWebBrowserLogger : IWebBrowserLogger
+    {
+        public void Debug(object message)
+        {
+            Logger.Debug(message.ToString());
+        }
+
+        public void Warn(object message)
+        {
+            Logger.Warn(message.ToString());
+        }
+
+        public void Error(object message)
+        {
+            Logger.Error(message.ToString());
+        }
+    }
+}
--- Team-Capture/Assets/Scripts/UI/MOTD/MOTDUI.cs
+++ Team-Capture/Assets/Scripts/UI/MOTD/MOTDUI.cs
@@ -5,14 +5,12 @@
 // For more details see the LICENSE file.
 
 using System;
-using Cysharp.Threading.Tasks;
 using Team_Capture.Core.Networking;
+using Team_Capture.Logging;
 using Team_Capture.UserManagement;
 using TMPro;
 using UnityEngine;
 using UnityWebBrowser;
-using Logger = Team_Capture.Logging.Logger;
-using UniTask = Team_Capture.Integrations.UniTask.UniTask;
 
 namespace Team_Capture.UI.MOTD
 {
@@ -40,12 +38,12 @@ namespace Team_Capture.UI.MOTD
 
 		    if (serverConfig.MotdMode == Server.ServerMOTDMode.WebOnly || serverConfig.MotdMode == Server.ServerMOTDMode.WebWithTextBackup && Client.ClientMotdMode == Client.ClientMOTDMode.WebSupport)
 		    {
-			    webBrowserUI.browserClient.ReplaceLogger(Logger.UnityLogger);
+			    webBrowserUI.browserClient.Logger = new TCWebBrowserLogger();
 			    webBrowserUI.browserClient.initialUrl = serverConfig.MotdUrl.String;
+			    webBrowserUI.browserClient.OnLoadFinish += OnLoadFinish;
 
 			    motdTextScroll.SetActive(false);
 			    webBrowserUI.gameObject.SetActive(true);
-			    SendJs().Forget();
 		    }
 
 		    else if (serverConfig.MotdMode == Server.ServerMOTDMode.TextOnly || serverConfig.MotdMode == Server.ServerMOTDMode.WebWithTextBackup)
@@ -62,21 +60,19 @@ namespace Team_Capture.UI.MOTD
 		    onCloseAction = onClose;
 	    }
 
-	    public void CloseMOTD()
+		private void OnLoadFinish(string url)
+		{
+			string javaScriptCode =
+				$"class UserDetails {{ constructor(username) {{ this.UserName = username; }} }}" +
+				$"let userDetails = new UserDetails(\"{User.GetActiveUser().UserName}\");";
+
+			webBrowserUI.ExecuteJs(javaScriptCode);
+		}
+
+		public void CloseMOTD()
 	    {
 			onCloseAction.Invoke();
 			Destroy(gameObject);
 	    }
-
-	    private async UniTaskVoid SendJs()
-	    {
-		    await UniTask.WaitUntil(() => webBrowserUI.browserClient.IsRunning);
-
-		    string javaScriptCode =
-			    $"class UserDetails {{ constructor(username) {{ this.UserName = username; }} }}" +
-			    $"let userDetails = new UserDetails(\"{User.GetActiveUser().UserName}\");";
-
-		    webBrowserUI.ExecuteJs(javaScriptCode);
-	    } 
     }
 }
