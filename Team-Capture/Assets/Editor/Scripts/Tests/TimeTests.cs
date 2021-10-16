d107cf283ef7bb800b35d1c63fd1a6974e67e2a6
Voltstro <me@voltstro.dev>
2021-10-16 20:14:46 +1000

    Add death screen

---
+++ Team-Capture/Assets/Editor/Scripts/Tests/TimeTests.cs
@@ -0,0 +1,30 @@
+﻿// Team-Capture
+// Copyright (C) 2019-2021 Voltstro-Studios
+// 
+// This project is governed by the AGPLv3 License.
+// For more details see the LICENSE file.
+
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Team_Capture.Helper;
+
+namespace Team_Capture.Editor.Tests
+{
+    public class DeathScreenCountdownTest
+    {
+        [Test]
+        public async Task CountDownTest()
+        {
+            const int time = 5;
+            int currentTime = 0;
+
+            await TimeHelper.CountDown(time, () =>
+            {
+                currentTime++;
+            });
+            
+            Assert.AreEqual(time, currentTime);
+        }
+    }
+}
---
+++ Team-Capture/Assets/Scripts/UI/DeathScreen.cs
@@ -0,0 +1,13 @@
+﻿// Team-Capture
+// Copyright (C) 2019-2021 Voltstro-Studios
+// 
+// This project is governed by the AGPLv3 License.
+// For more details see the LICENSE file.
+
+namespace Team_Capture.UI
+{
+    public class DeathScreen
+    {
+        
+    }
+}
