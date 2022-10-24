4ceec477ea97c95004dd94fa42b6c4717674dbea
Voltstro <me@voltstro.dev>
2022-10-25 01:15:59 +1000

    Cleanup generic dictionary Update dm_ditch scene settings

--- src/Team-Capture/Assets/Editor/Scripts/CustomEditors/GenericDictionaryDrawer.cs
+++ src/Team-Capture/Assets/Editor/Scripts/CustomEditors/GenericDictionaryDrawer.cs
@@ -1,59 +1,65 @@
+// Team-Capture
+// Copyright (c) 2019-2022 Voltstro-Studios
+// 
+// This project is governed by the AGPLv3 License.
+// For more details see the LICENSE file.
+
 using Team_Capture.Collections;
 using UnityEditor;
 using UnityEngine;
 
 namespace Team_Capture.Editor.CustomEditors
 {
+    //From:
+    //https://github.com/upscalebaby/generic-serializable-dictionary/blob/70133ea96240b5626711dc821039a84f05ca85e6/Assets/Editor/GenericDictionaryPropertyDrawer.cs
+    
     /// <summary>
     /// Draws the dictionary and a warning-box if there are duplicate keys.
     /// </summary>
     [CustomPropertyDrawer(typeof(GenericDictionary<,>))]
     public class GenericDictionaryDrawer : PropertyDrawer
     {
-        private static float lineHeight = EditorGUIUtility.singleLineHeight;
-        private static float vertSpace = EditorGUIUtility.standardVerticalSpacing;
-        private const float warningBoxHeight = 1.5f;
+        private static readonly float LineHeight = EditorGUIUtility.singleLineHeight;
+        private static readonly float VertSpace = EditorGUIUtility.standardVerticalSpacing;
+        private const float WarningBoxHeight = 1.5f;
 
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
-            // Draw list of key/value pairs.
-            var list = property.FindPropertyRelative("list");
+            //Draw list of key/value pairs.
+            SerializedProperty list = property.FindPropertyRelative("list");
             EditorGUI.PropertyField(position, list, label, true);
 
             // Draw key collision warning.
-            var keyCollision = property.FindPropertyRelative("keyCollision").boolValue;
-            if (keyCollision)
-            {
-                position.y += EditorGUI.GetPropertyHeight(list, true);
-                if (!list.isExpanded)
-                {
-                    position.y += vertSpace;
-                }
-                position.height = lineHeight * warningBoxHeight;
-                position = EditorGUI.IndentedRect(position);
-                EditorGUI.HelpBox(position, "Duplicate keys will not be serialized.", MessageType.Warning);
-            }
+            bool keyCollision = property.FindPropertyRelative("keyCollision").boolValue;
+            if (!keyCollision)
+                return;
+            
+            position.y += EditorGUI.GetPropertyHeight(list, true);
+            if (!list.isExpanded)
+                position.y += VertSpace;
+            
+            position.height = LineHeight * WarningBoxHeight;
+            position = EditorGUI.IndentedRect(position);
+            
+            EditorGUI.HelpBox(position, "Duplicate keys will not be serialized.", MessageType.Warning);
         }
 
         public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
         {
-            // Height of KeyValue list.
+            //Height of KeyValue list.
             float height = 0f;
-            var list = property.FindPropertyRelative("list");
+            SerializedProperty list = property.FindPropertyRelative("list");
             height += EditorGUI.GetPropertyHeight(list, true);
 
-            // Height of key collision warning.
+            //Height of key collision warning.
             bool keyCollision = property.FindPropertyRelative("keyCollision").boolValue;
-            if (keyCollision)
-            {
-                height += warningBoxHeight * lineHeight;
-                if (!list.isExpanded)
-                {
-                    height += vertSpace;
-                }
-            }
+            if (!keyCollision)
+                return height;
+            
+            height += WarningBoxHeight * LineHeight;
+            if (!list.isExpanded)
+                height += VertSpace;
             return height;
         }
     }
-
 }
--- src/Team-Capture/Assets/Scripts/Collections/GenericDictionary.cs
+++ src/Team-Capture/Assets/Scripts/Collections/GenericDictionary.cs
@@ -1,3 +1,9 @@
+// Team-Capture
+// Copyright (c) 2019-2022 Voltstro-Studios
+// 
+// This project is governed by the AGPLv3 License.
+// For more details see the LICENSE file.
+
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -5,6 +11,9 @@ using UnityEngine;
 
 namespace Team_Capture.Collections
 {
+    //From:
+    //https://github.com/upscalebaby/generic-serializable-dictionary/blob/70133ea96240b5626711dc821039a84f05ca85e6/Assets/Scripts/GenericDictionary.cs
+    
     /// <summary>
     /// Generic Serializable Dictionary for Unity 2020.1 and above.
     /// Simply declare your key/value types and you're good to go - zero boilerplate.
@@ -14,11 +23,10 @@ namespace Team_Capture.Collections
     {
         // Internal
         [SerializeField]
-        private List<KeyValuePair> list = new List<KeyValuePair>();
-        [SerializeField, HideInInspector]
-        private Dictionary<TKey, int> indexByKey = new Dictionary<TKey, int>();
-        [SerializeField, HideInInspector]
-        private Dictionary<TKey, TValue> dict = new Dictionary<TKey, TValue>();
+        private List<KeyValuePair> list = new();
+        
+        private Dictionary<TKey, int> indexByKey = new();
+        private Dictionary<TKey, TValue> dict = new();
 
 #pragma warning disable 0414
         [SerializeField, HideInInspector]
@@ -28,30 +36,32 @@ namespace Team_Capture.Collections
         [Serializable]
         private struct KeyValuePair
         {
-            public TKey Key;
-            public TValue Value;
-            public KeyValuePair(TKey Key, TValue Value)
+            public TKey key;
+            public TValue value;
+            
+            public KeyValuePair(TKey key, TValue value)
             {
-                this.Key = Key;
-                this.Value = Value;
+                this.key = key;
+                this.value = value;
             }
         }
 
-        // Lists are serialized natively by Unity, no custom implementation needed.
+        //Lists are serialized natively by Unity, no custom implementation needed.
         public void OnBeforeSerialize() { }
 
-        // Populate dictionary with pairs from list and flag key-collisions.
+        //Populate dictionary with pairs from list and flag key-collisions.
         public void OnAfterDeserialize()
         {
             dict.Clear();
             indexByKey.Clear();
             keyCollision = false;
+            
             for (int i = 0; i < list.Count; i++)
             {
-                var key = list[i].Key;
+                TKey key = list[i].key;
                 if (key != null && !ContainsKey(key))
                 {
-                    dict.Add(key, list[i].Value);
+                    dict.Add(key, list[i].value);
                     indexByKey.Add(key, i);
                 }
                 else
@@ -61,7 +71,7 @@ namespace Team_Capture.Collections
             }
         }
 
-        // IDictionary
+        //IDictionary
         public TValue this[TKey key]
         {
             get => dict[key];
@@ -70,7 +80,7 @@ namespace Team_Capture.Collections
                 dict[key] = value;
                 if (indexByKey.ContainsKey(key))
                 {
-                    var index = indexByKey[key];
+                    int index = indexByKey[key];
                     list[index] = new KeyValuePair(key, value);
                 }
                 else
@@ -93,25 +103,23 @@ namespace Team_Capture.Collections
 
         public bool ContainsKey(TKey key) => dict.ContainsKey(key);
 
-        public bool Remove(TKey key) 
+        public bool Remove(TKey key)
         {
-            if (dict.Remove(key))
-            {
-                var index = indexByKey[key];
-                list.RemoveAt(index);
-                UpdateIndexLookup(index);
-                indexByKey.Remove(key);
-                return true;
-            }
-            else
-            {
+            if (!dict.Remove(key))
                 return false;
-            }
+            
+            int index = indexByKey[key];
+            list.RemoveAt(index);
+            UpdateIndexLookup(index);
+            indexByKey.Remove(key);
+            return true;
+
         }
 
         private void UpdateIndexLookup(int removedIndex) {
-            for (int i = removedIndex; i < list.Count; i++) {
-                var key = list[i].Key;
+            for (int i = removedIndex; i < list.Count; i++)
+            {
+                TKey key = list[i].key;
                 indexByKey[key]--;
             }
         }
@@ -136,15 +144,7 @@ namespace Team_Capture.Collections
 
         public bool Contains(KeyValuePair<TKey, TValue> pair)
         {
-            TValue value;
-            if (dict.TryGetValue(pair.Key, out value))
-            {
-                return EqualityComparer<TValue>.Default.Equals(value, pair.Value);
-            }
-            else
-            {
-                return false;
-            }
+            return dict.TryGetValue(pair.Key, out TValue value) && EqualityComparer<TValue>.Default.Equals(value, pair.Value);
         }
 
         public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
@@ -152,11 +152,11 @@ namespace Team_Capture.Collections
             if (array == null)
                 throw new ArgumentException("The array cannot be null.");
             if (arrayIndex < 0)
-                throw new ArgumentOutOfRangeException("The starting array index cannot be negative.");
+                throw new ArgumentOutOfRangeException(nameof(arrayIndex), "The starting array index cannot be negative.");
             if (array.Length - arrayIndex < dict.Count)
                 throw new ArgumentException("The destination array has fewer elements than the collection.");
 
-            foreach (var pair in dict)
+            foreach (KeyValuePair<TKey, TValue> pair in dict)
             {
                 array[arrayIndex] = pair;
                 arrayIndex++;
@@ -165,16 +165,11 @@ namespace Team_Capture.Collections
 
         public bool Remove(KeyValuePair<TKey, TValue> pair)
         {
-            TValue value;
-            if (dict.TryGetValue(pair.Key, out value))
-            {
-                bool valueMatch = EqualityComparer<TValue>.Default.Equals(value, pair.Value);
-                if (valueMatch)
-                {
-                    return Remove(pair.Key);
-                }
-            }
-            return false;
+            if (!dict.TryGetValue(pair.Key, out TValue value))
+                return false;
+            
+            bool valueMatch = EqualityComparer<TValue>.Default.Equals(value, pair.Value);
+            return valueMatch && Remove(pair.Key);
         }
 
         // IEnumerable
